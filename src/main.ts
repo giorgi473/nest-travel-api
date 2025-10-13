b85db754406727cadf58fb313a50ceb7ecd314ca
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-10-13 22:18:37 +0400

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -23,11 +23,11 @@ async function bootstrap() {
     '/uploads',
     express.static(path.join(__dirname, '..', 'public', 'uploads')),
   );
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  // app.enableCors({
+  //   origin: 'http://localhost:3000',
+  //   methods: 'GET,POST,PUT,DELETE',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
 
   await app.listen(8000);
 }
