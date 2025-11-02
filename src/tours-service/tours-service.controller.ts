499e40e40087744f8f125857ae91b40b0c2c7a2c
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-11-02 14:06:11 +0400

    Initial commit

--- src/tours-service/tours-service.controller.ts
+++ src/tours-service/tours-service.controller.ts
@@ -48,95 +48,6 @@
 //   }
 // }
 
-// ------------------------------------
-
-// import {
-//   Controller,
-//   Get,
-//   Post,
-//   Body,
-//   Param,
-//   Put,
-//   Delete,
-//   HttpCode,
-//   HttpStatus,
-// } from '@nestjs/common';
-// import { ToursServiceService } from './tours-service.service';
-// import { CreateCardDto, CreatePopularTourDto } from './dto/create-tour.dto';
-// import { UpdateCardDto } from './dto/update-tours-service.dto';
-
-// @Controller('tours')
-// export class ToursServiceController {
-//   constructor(private readonly toursService: ToursServiceService) {}
-
-//   // 📌 CARD ENDPOINTS
-
-//   @Post()
-//   @HttpCode(HttpStatus.CREATED)
-//   createCard(@Body() createCardDto: CreateCardDto) {
-//     return this.toursService.createCard(createCardDto);
-//   }
-
-//   @Get()
-//   getAllCards() {
-//     return this.toursService.getAllCards();
-//   }
-
-//   @Get(':id')
-//   getCardById(@Param('id') id: number) {
-//     return this.toursService.getCardById(id);
-//   }
-
-//   @Put(':id')
-//   updateCard(@Param('id') id: number, @Body() updateCardDto: UpdateCardDto) {
-//     return this.toursService.updateCard(id, updateCardDto);
-//   }
-
-//   @Delete(':id')
-//   deleteCard(@Param('id') id: number) {
-//     return this.toursService.deleteCard(id);
-//   }
-
-//   // 📌 POPULAR TOUR ENDPOINTS
-
-//   @Post('cards/:cardId/tours')
-//   @HttpCode(HttpStatus.CREATED)
-//   createPopularTour(
-//     @Param('cardId') cardId: number,
-//     @Body() createTourDto: CreatePopularTourDto,
-//   ) {
-//     return this.toursService.createPopularTour(cardId, createTourDto);
-//   }
-
-//   @Get('cards/:cardId/tours')
-//   getPopularToursByCard(@Param('cardId') cardId: number) {
-//     return this.toursService.getPopularToursByCard(cardId);
-//   }
-
-//   @Put('tours/:tourId')
-//   updatePopularTour(
-//     @Param('tourId') tourId: number,
-//     @Body() updateTourDto: CreatePopularTourDto,
-//   ) {
-//     return this.toursService.updatePopularTour(tourId, updateTourDto);
-//   }
-
-//   @Delete('tours/:tourId')
-//   deletePopularTour(@Param('tourId') tourId: number) {
-//     return this.toursService.deletePopularTour(tourId);
-//   }
-
-//   // 📌 SEED ENDPOINT
-
-//   @Post('seed')
-//   @HttpCode(HttpStatus.CREATED)
-//   seedDatabase() {
-//     const cardSliderImages = [
-//       // თქვენი მასივი აქ...
-//     ];
-//     return this.toursService.seedDatabase(cardSliderImages);
-//   }
-// }
 import {
   Controller,
   Get,
@@ -147,11 +58,7 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
-  UseInterceptors,
-  UploadedFile,
-  BadRequestException,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
 import { ToursServiceService } from './tours-service.service';
 import { CreateCardDto, CreatePopularTourDto } from './dto/create-tour.dto';
 import { UpdateCardDto } from './dto/update-tours-service.dto';
@@ -164,21 +71,8 @@ export class ToursServiceController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  @UseInterceptors(FileInterceptor('image'))
-  async createCard(
-    @Body() createCardDto: CreateCardDto,
-    @UploadedFile() file?: Express.Multer.File,
-  ) {
-    if (!file) {
-      throw new BadRequestException('Image file is required');
-    }
-
-    const base64Image = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
-
-    return this.toursService.createCard(
-      { ...createCardDto, image: '' },
-      base64Image,
-    );
+  createCard(@Body() createCardDto: CreateCardDto) {
+    return this.toursService.createCard(createCardDto);
   }
 
   @Get()
@@ -192,19 +86,8 @@ export class ToursServiceController {
   }
 
   @Put(':id')
-  @UseInterceptors(FileInterceptor('image'))
-  async updateCard(
-    @Param('id') id: number,
-    @Body() updateCardDto: UpdateCardDto,
-    @UploadedFile() file?: Express.Multer.File,
-  ) {
-    let base64Image: string | undefined;
-
-    if (file) {
-      base64Image = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
-    }
-
-    return this.toursService.updateCard(id, updateCardDto, base64Image);
+  updateCard(@Param('id') id: number, @Body() updateCardDto: UpdateCardDto) {
+    return this.toursService.updateCard(id, updateCardDto);
   }
 
   @Delete(':id')
@@ -216,23 +99,11 @@ export class ToursServiceController {
 
   @Post('cards/:cardId/tours')
   @HttpCode(HttpStatus.CREATED)
-  @UseInterceptors(FileInterceptor('image'))
-  async createPopularTour(
+  createPopularTour(
     @Param('cardId') cardId: number,
     @Body() createTourDto: CreatePopularTourDto,
-    @UploadedFile() file?: Express.Multer.File,
   ) {
-    if (!file) {
-      throw new BadRequestException('Image file is required');
-    }
-
-    const base64Image = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
-
-    return this.toursService.createPopularTour(
-      cardId,
-      { ...createTourDto, image: '' },
-      base64Image,
-    );
+    return this.toursService.createPopularTour(cardId, createTourDto);
   }
 
   @Get('cards/:cardId/tours')
@@ -241,23 +112,11 @@ export class ToursServiceController {
   }
 
   @Put('tours/:tourId')
-  @UseInterceptors(FileInterceptor('image'))
-  async updatePopularTour(
+  updatePopularTour(
     @Param('tourId') tourId: number,
     @Body() updateTourDto: CreatePopularTourDto,
-    @UploadedFile() file?: Express.Multer.File,
   ) {
-    let base64Image: string | undefined;
-
-    if (file) {
-      base64Image = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
-    }
-
-    return this.toursService.updatePopularTour(
-      tourId,
-      updateTourDto,
-      base64Image,
-    );
+    return this.toursService.updatePopularTour(tourId, updateTourDto);
   }
 
   @Delete('tours/:tourId')
