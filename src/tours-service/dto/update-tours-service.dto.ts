20f36edd58203b721d8e834534eadf348ca099ba
giorgi473 <giorgi.kavtaradze2000@mail.ru>
2025-11-02 14:19:24 +0400

    Updated Nest.js

--- src/tours-service/dto/update-tours-service.dto.ts
+++ src/tours-service/dto/update-tours-service.dto.ts
@@ -1,8 +1,3 @@
-// import { PartialType } from '@nestjs/mapped-types';
-// import { CreateToursServiceDto } from './create-tours-service.dto';
-
-// export class UpdateToursServiceDto extends PartialType(CreateToursServiceDto) {}
-
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-tour.dto';
 
--- src/tours-service/tours-service.module.ts
+++ src/tours-service/tours-service.module.ts
@@ -1,32 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ToursServiceService } from './tours-service.service';
-// import { ToursServiceController } from './tours-service.controller';
-// import { ToursServiceEntity } from './entities/tours-service.entity';
-// import { DestinationEntity } from './entities/destination.entity';
-
-// @Module({
-//   imports: [TypeOrmModule.forFeature([ToursServiceEntity, DestinationEntity])],
-//   controllers: [ToursServiceController],
-//   providers: [ToursServiceService],
-// })
-// export class ToursServiceModule {}
-
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ToursServiceService } from './tours-service.service';
-// import { ToursServiceController } from './tours-service.controller';
-// import { Card } from './entities/card.entity';
-// import { PopularTour } from './entities/popular-tour.entity';
-
-// @Module({
-//   imports: [TypeOrmModule.forFeature([Card, PopularTour])],
-//   providers: [ToursServiceService],
-//   controllers: [ToursServiceController],
-//   exports: [ToursServiceService],
-// })
-// export class ToursServiceModule {}
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ToursServiceService } from './tours-service.service';
@@ -42,4 +13,3 @@ import { CloudinaryModule } from '../cloudinary/cloudinary.module';
   exports: [ToursServiceService],
 })
 export class ToursServiceModule {}
-
